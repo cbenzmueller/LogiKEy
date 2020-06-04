5477f9da1c71110902db07fc786581d895e8e35a
Benzmüller, Christoph <cbenzmueller@zedat.fu-berlin.de>
2020-06-04 16:02:19 +0200

    update

---
+++ PreferenceLogics/vanBenthemEtAl2009/PreferenceLogicBasics.thy
@@ -0,0 +1,98 @@
+theory PreferenceLogicBasics imports Main              
+begin 
+(*default settings for nitpick model finder*)
+nitpick_params[assms=true,user_axioms=true,show_all,expect=genuine,format=3] 
+
+(**** Embedding of "a basic modal preference language" in HOL  ****)
+
+(*Type declarations and type abbreviations*)
+typedecl i (*Possible worlds*)  
+typedecl e (*Individuals*)      
+type_synonym \<sigma> = "i\<Rightarrow>bool" (*World-lifted propositions*)
+type_synonym \<mu> = "\<sigma>\<Rightarrow>\<sigma>" (*Unary modal connectives*)
+type_synonym \<nu> = "\<sigma>\<Rightarrow>\<sigma>\<Rightarrow>\<sigma>" (*Binary modal connectives*)
+type_synonym \<gamma> = "i\<Rightarrow>i\<Rightarrow>bool" (*Preference relations*)
+type_synonym \<pi> = "\<sigma>\<Rightarrow>bool" (*Set of world-lefted formulas*)
+
+consts BR::\<gamma> ("_\<preceq>_")  (*Betterness relation*)
+abbreviation SBR::\<gamma> ("_\<prec>_")  where  "v\<prec>w \<equiv> (v\<preceq>w) \<and> \<not>(w\<preceq>v)" (*Strict Betterness relation*)
+
+abbreviation reflexive :: "\<gamma>\<Rightarrow>bool" where "reflexive Rel \<equiv> \<forall>x. Rel x x"
+abbreviation transitive :: "\<gamma>\<Rightarrow>bool" where "transitive Rel \<equiv> \<forall>x y z. Rel x y \<and> Rel y z \<longrightarrow> Rel x z"
+abbreviation total :: "\<gamma>\<Rightarrow>bool" where "total Rel \<equiv> \<forall>x y. Rel x y \<or> Rel y x"
+abbreviation irreflexive :: "\<gamma>\<Rightarrow>bool" where "irreflexive Rel \<equiv> \<forall>x. \<not>(Rel x x)"
+
+axiomatization where 
+  reflBR:    "reflexive BR"  and 
+  transBR: "transitive BR"
+
+lemma "irreflexive SBR" by simp
+lemma "transitive SBR" using transBR by blast
+lemma "total SBR \<longrightarrow> total BR" by auto
+
+(*Modal logic connectives (operating on truth-sets)*)
+abbreviation c1::\<sigma> ("\<^bold>\<bottom>") where "\<^bold>\<bottom> \<equiv> \<lambda>w. False"
+abbreviation c2::\<sigma> ("\<^bold>\<top>") where "\<^bold>\<top> \<equiv> \<lambda>w. True"
+abbreviation c3::\<mu> ("\<^bold>\<not>_") where "\<^bold>\<not>\<phi> \<equiv> \<lambda>w.\<not>(\<phi> w)"
+abbreviation c4::\<nu> ("_\<^bold>\<and>_") where "\<phi>\<^bold>\<and>\<psi> \<equiv> \<lambda>w.(\<phi> w)\<and>(\<psi> w)"   
+abbreviation c5::\<nu> ("_\<^bold>\<or>_") where "\<phi>\<^bold>\<or>\<psi> \<equiv> \<lambda>w.(\<phi> w)\<or>(\<psi> w)"
+abbreviation c6::\<nu> ("_\<^bold>\<rightarrow>_") where "\<phi>\<^bold>\<rightarrow>\<psi> \<equiv> \<lambda>w.(\<phi> w)\<longrightarrow>(\<psi> w)"  
+abbreviation c7::\<nu> ("_\<^bold>\<leftrightarrow>_") where "\<phi>\<^bold>\<leftrightarrow>\<psi> \<equiv> \<lambda>w.(\<phi> w)\<longleftrightarrow>(\<psi> w)"
+abbreviation c8::\<mu> ("\<^bold>\<box>\<^sup>\<preceq>_") where "\<^bold>\<box>\<^sup>\<preceq>\<phi> \<equiv> \<lambda>w.\<forall>v.(w\<preceq>v)\<longrightarrow>(\<phi> v)"
+abbreviation c9::\<mu> ("\<^bold>\<diamond>\<^sup>\<preceq>_") where "\<^bold>\<diamond>\<^sup>\<preceq>\<phi> \<equiv> \<lambda>w.\<exists>v.(w\<preceq>v)\<and>(\<phi> v)"
+abbreviation c10::\<mu> ("\<^bold>\<box>\<^sup>\<prec>_") where "\<^bold>\<box>\<^sup>\<prec>\<phi> \<equiv> \<lambda>w.\<forall>v.(w\<prec>v)\<longrightarrow>(\<phi> v)"
+abbreviation c11::\<mu> ("\<^bold>\<diamond>\<^sup>\<prec>_") where "\<^bold>\<diamond>\<^sup>\<prec>\<phi> \<equiv> \<lambda>w.\<exists>v.(w\<prec>v)\<and>(\<phi> v)"
+abbreviation c12::\<mu> ("\<^bold>E_") where "\<^bold>E\<phi> \<equiv> \<lambda>w.\<exists>v.(\<phi> v)"  (*Global existence modality*)
+abbreviation c13::\<mu> ("\<^bold>A_") where "\<^bold>A\<phi> \<equiv> \<lambda>w.\<forall>v.(\<phi> v)"  (*Global universal modality*)
+
+(*Meta-logical predicate for global validity*)
+abbreviation g1::\<pi> ("\<lfloor>_\<rfloor>") where "\<lfloor>\<psi>\<rfloor> \<equiv>  \<forall>w. \<psi> w"
+
+(*Some tests: dualities*)
+lemma "\<lfloor>(\<^bold>\<diamond>\<^sup>\<preceq>\<phi>) \<^bold>\<leftrightarrow> (\<^bold>\<not>\<^bold>\<box>\<^sup>\<preceq>\<^bold>\<not>\<phi>)\<rfloor>" by blast
+lemma "\<lfloor>(\<^bold>\<diamond>\<^sup>\<prec>\<phi>) \<^bold>\<leftrightarrow> (\<^bold>\<not>\<^bold>\<box>\<^sup>\<prec>\<^bold>\<not>\<phi>)\<rfloor>" by blast
+lemma "\<lfloor>(\<^bold>A\<phi>) \<^bold>\<leftrightarrow> (\<^bold>\<not>\<^bold>E\<^bold>\<not>\<phi>)\<rfloor>" by blast
+
+(**** Section 3: A basic modal preference language ****)
+
+(*Definition 5*)
+abbreviation leqEE::\<nu> ("_\<preceq>\<^sub>E\<^sub>E_")  where  "(\<phi> \<preceq>\<^sub>E\<^sub>E \<psi>) u \<equiv> \<exists>s. \<exists>t. (\<phi> s) \<and> (\<psi> t) \<and> (s\<preceq>t)" 
+abbreviation leqAE::\<nu> ("_\<preceq>\<^sub>A\<^sub>E_")  where  "(\<phi> \<preceq>\<^sub>A\<^sub>E \<psi>) u \<equiv> \<forall>s. \<exists>t. (\<phi> s) \<longrightarrow> (\<psi> t) \<and> (s\<preceq>t)" 
+abbreviation leEE::\<nu>   ("_\<prec>\<^sub>E\<^sub>E_")  where  "(\<phi> \<prec>\<^sub>E\<^sub>E \<psi>) u \<equiv> \<exists>s. \<exists>t. (\<phi> s) \<and> (\<psi> t) \<and> (s\<prec>t)" 
+abbreviation leAE::\<nu>   ("_\<prec>\<^sub>A\<^sub>E_")  where  "(\<phi> \<prec>\<^sub>A\<^sub>E \<psi>) u \<equiv> \<forall>s. \<exists>t. (\<phi> s) \<longrightarrow> (\<psi> t) \<and> (s\<prec>t)" 
+abbreviation leAA::\<nu>   ("_\<prec>\<^sub>A\<^sub>A_")  where  "(\<phi> \<prec>\<^sub>A\<^sub>A \<psi>) u \<equiv> \<forall>s. \<forall>t. (\<phi> s) \<and> (\<psi> t) \<longrightarrow> (s\<prec>t)" 
+abbreviation gEA::\<nu>    ("_\<succ>\<^sub>E\<^sub>A_")  where  "(\<phi> \<succ>\<^sub>E\<^sub>A \<psi>) u \<equiv> \<exists>s. \<forall>t. (\<phi> s) \<and> (\<psi> t) \<longrightarrow> (t\<prec>s)" 
+abbreviation leqAA::\<nu> ("_\<preceq>\<^sub>A\<^sub>A_")  where  "(\<phi> \<preceq>\<^sub>A\<^sub>A \<psi>) u \<equiv> \<forall>s. \<forall>t. (\<phi> s) \<and> (\<psi> t) \<longrightarrow> (s\<preceq>t)" 
+abbreviation geqEA::\<nu> ("_\<succeq>\<^sub>E\<^sub>A_")  where  "(\<phi> \<succeq>\<^sub>E\<^sub>A \<psi>) u \<equiv> \<exists>s. \<forall>t. (\<phi> s) \<and> (\<psi> t) \<longrightarrow> (t\<preceq>s)" 
+
+abbreviation leqEE'::\<nu> ("_\<^bold>\<preceq>\<^sub>E\<^sub>E_")  where  "\<phi> \<^bold>\<preceq>\<^sub>E\<^sub>E \<psi> \<equiv> \<^bold>E(\<phi> \<^bold>\<and> \<^bold>\<diamond>\<^sup>\<preceq>\<psi>)" 
+abbreviation leqAE'::\<nu> ("_\<^bold>\<preceq>\<^sub>A\<^sub>E_")  where  "\<phi> \<^bold>\<preceq>\<^sub>A\<^sub>E \<psi> \<equiv> \<^bold>A(\<phi> \<^bold>\<rightarrow> \<^bold>\<diamond>\<^sup>\<preceq>\<psi>)" 
+abbreviation leEE'::\<nu>   ("_\<^bold>\<prec>\<^sub>E\<^sub>E_")  where  "\<phi> \<^bold>\<prec>\<^sub>E\<^sub>E \<psi> \<equiv> \<^bold>E(\<phi> \<^bold>\<and> \<^bold>\<diamond>\<^sup>\<prec>\<psi>)" 
+abbreviation leAA'::\<nu>   ("_\<^bold>\<prec>\<^sub>A\<^sub>A_")  where  "\<phi> \<^bold>\<prec>\<^sub>A\<^sub>A \<psi> \<equiv> \<^bold>A(\<psi> \<^bold>\<rightarrow> \<^bold>\<box>\<^sup>\<preceq>\<^bold>\<not>\<phi>)" 
+abbreviation gEA'::\<nu>    ("_\<^bold>\<succ>\<^sub>E\<^sub>A_")  where  "\<phi> \<^bold>\<succ>\<^sub>E\<^sub>A \<psi> \<equiv> \<^bold>E(\<phi> \<^bold>\<and> \<^bold>\<box>\<^sup>\<preceq>\<^bold>\<not>\<psi>)" 
+abbreviation leqAA'::\<nu> ("_\<^bold>\<preceq>\<^sub>A\<^sub>A_")  where  "\<phi> \<^bold>\<preceq>\<^sub>A\<^sub>A \<psi> \<equiv> \<^bold>A(\<phi> \<^bold>\<and> \<^bold>\<box>\<^sup>\<prec>\<^bold>\<not>\<psi>)" 
+abbreviation geqEA'::\<nu> ("_\<^bold>\<succeq>\<^sub>E\<^sub>A_") where  "\<phi> \<^bold>\<succeq>\<^sub>E\<^sub>A \<psi> \<equiv> \<^bold>E(\<phi> \<^bold>\<and> \<^bold>\<box>\<^sup>\<prec>\<^bold>\<not>\<psi>)" 
+abbreviation leAE'::\<nu>   ("_\<^bold>\<prec>\<^sub>A\<^sub>E_")  where  "\<phi> \<^bold>\<prec>\<^sub>A\<^sub>E \<psi> \<equiv> \<^bold>A(\<phi> \<^bold>\<rightarrow> \<^bold>\<diamond>\<^sup>\<prec>\<psi>)" 
+
+(**** Section 5: Equality-based Ceteris Paribus Preference Logic ****)
+abbreviation elem::"\<sigma>\<Rightarrow>\<pi>\<Rightarrow>bool" ("_\<^bold>\<in>_")  where  "\<phi> \<^bold>\<in> \<Gamma> \<equiv> \<Gamma> \<phi>" 
+abbreviation subseteq::"\<pi>\<Rightarrow>\<pi>\<Rightarrow>bool" ("_\<^bold>\<subseteq>_")  where  "\<Gamma> \<^bold>\<subseteq> \<Gamma>' \<equiv> \<forall>\<phi>. (\<phi> \<^bold>\<in> \<Gamma>) \<longrightarrow> (\<phi> \<^bold>\<in> \<Gamma>')" 
+abbreviation union::"\<pi>\<Rightarrow>\<pi>\<Rightarrow>\<pi>" ("_\<^bold>\<union>_")  where  "\<Gamma> \<^bold>\<union> \<Gamma>' \<equiv> \<lambda>\<phi>. (\<phi> \<^bold>\<in> \<Gamma>) \<or> (\<phi> \<^bold>\<in> \<Gamma>')" 
+abbreviation mkSet::"\<sigma>\<Rightarrow>\<pi>" ("\<^bold>{_\<^bold>}")  where  "\<^bold>{\<phi>\<^bold>} \<equiv> \<lambda>x. x=\<phi>" 
+abbreviation emptySet ("\<^bold>\<emptyset>") where "\<^bold>\<emptyset> \<equiv> (\<lambda> \<psi>. False)"
+
+abbreviation c14::"i\<Rightarrow>\<pi>\<Rightarrow>i\<Rightarrow>bool" ("_ \<^bold>\<equiv>\<^sub>_ _") where "w \<^bold>\<equiv>\<^sub>\<Gamma> v \<equiv> \<forall>\<phi>. (\<phi> \<^bold>\<in> \<Gamma>) \<longrightarrow> ((\<phi> w) \<longleftrightarrow> (\<phi> v))"
+abbreviation c15::"i\<Rightarrow>\<pi>\<Rightarrow>i\<Rightarrow>bool" ("_ \<^bold>\<unlhd>\<^sub>_ _") where "w \<^bold>\<unlhd>\<^sub>\<Gamma> v \<equiv>  (w \<preceq> v) \<and> (w \<^bold>\<equiv>\<^sub>\<Gamma> v)"
+abbreviation c16::"i\<Rightarrow>\<pi>\<Rightarrow>i\<Rightarrow>bool" ("_ \<^bold>\<lhd>\<^sub>_ _") where "w \<^bold>\<lhd>\<^sub>\<Gamma> v \<equiv>  (w \<prec> v) \<and> (w \<^bold>\<equiv>\<^sub>\<Gamma> v)"
+abbreviation c17::"\<pi>\<Rightarrow>\<sigma>\<Rightarrow>\<sigma>" ("\<^bold>\<langle>_\<^bold>\<rangle>\<^sup>\<preceq>_") where "\<^bold>\<langle>\<Gamma>\<^bold>\<rangle>\<^sup>\<preceq>\<phi> \<equiv> \<lambda>w.\<exists>v. (w \<^bold>\<unlhd>\<^sub>\<Gamma> v) \<and> (\<phi> v)"
+abbreviation c18::"\<pi>\<Rightarrow>\<sigma>\<Rightarrow>\<sigma>" ("[_]\<^sup>\<preceq>_") where "[\<Gamma>]\<^sup>\<preceq>\<phi> \<equiv> \<lambda>w.\<forall>v. (w \<^bold>\<unlhd>\<^sub>\<Gamma> v) \<longrightarrow> (\<phi> v)"
+abbreviation c19::"\<pi>\<Rightarrow>\<sigma>\<Rightarrow>\<sigma>" ("\<^bold>\<langle>_\<^bold>\<rangle>\<^sup>\<prec>_") where "\<^bold>\<langle>\<Gamma>\<^bold>\<rangle>\<^sup>\<prec>\<phi> \<equiv> \<lambda>w.\<exists>v. (w \<^bold>\<lhd>\<^sub>\<Gamma> v) \<and> (\<phi> v)"
+abbreviation c20::"\<pi>\<Rightarrow>\<sigma>\<Rightarrow>\<sigma>" ("[_]\<^sup>\<prec>_") where "[\<Gamma>]\<^sup>\<prec>\<phi> \<equiv> \<lambda>w.\<forall>v. (w \<^bold>\<lhd>\<^sub>\<Gamma> v) \<longrightarrow> (\<phi> v)"
+abbreviation c21::"\<pi>\<Rightarrow>\<sigma>\<Rightarrow>\<sigma>" ("\<^bold>\<langle>_\<^bold>\<rangle>_") where "\<^bold>\<langle>\<Gamma>\<^bold>\<rangle>\<phi> \<equiv> \<lambda>w.\<exists>v. (w \<^bold>\<equiv>\<^sub>\<Gamma> v) \<and> (\<phi> v)"
+abbreviation c22::"\<pi>\<Rightarrow>\<sigma>\<Rightarrow>\<sigma>" ("[_]_") where "[\<Gamma>]\<phi> \<equiv> \<lambda>w.\<forall>v. (w \<^bold>\<equiv>\<^sub>\<Gamma> v) \<longrightarrow> (\<phi> v)"
+
+(*Consistency confirmed by nitpick (trivial since only abbreviations were introduced)*)
+lemma True nitpick[satisfy,user_axioms] oops
+end
+
+
