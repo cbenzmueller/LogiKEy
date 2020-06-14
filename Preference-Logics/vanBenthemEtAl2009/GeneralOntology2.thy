4289923751c16c3eff39655ef286692644ff30c5
Benzmüller, Christoph <cbenzmueller@zedat.fu-berlin.de>
2020-06-14 10:56:36 +0200

    update

--- Preference-Logics/vanBenthemEtAl2009/GeneralOntology2.thy
+++ Preference-Logics/vanBenthemEtAl2009/GeneralOntology2.thy
@@ -1,41 +1,48 @@
-theory GeneralOntology2 imports ValueOntology2 (*Benzm.,Fuenmayor&Lomfeld, 2020*)               
-begin (**General World/Legal Knowledge**)
-
+theory GeneralOntology2  (*Benzmüller, Fuenmayor & Lomfeld, 2020*)  
+  imports ValueOntology2 
+begin 
+(**General World/Legal Knowledge**)
 abbreviation pref::\<nu>  ("_\<^bold>\<prec>_")  where  "\<phi> \<^bold>\<prec> \<psi> \<equiv> \<phi> \<^bold>\<prec>\<^sub>A\<^sub>A \<psi>"  
 
-(*legal/world knowledge: kinds of situations*)
-consts appObject::"\<sigma>"         (*appropriation of objects in general*)
-consts appAnimal::"\<sigma>"         (*appropriation of animals in general*)
-consts appWildAnimal::"\<sigma>"     (*appropriation of wild animals*)
-consts appDomesticAnimal::"\<sigma>" (*appropriation of domestic animals*)
+(*legal/world knowledge (LWK): kinds of situations*)
+consts appObject::"\<sigma>"      (*appropriation of objects in general*)
+consts appAnimal::"\<sigma>"      (*appropriation of animals in general*)
+consts appWildAnimal::"\<sigma>"  (*appropriation of wild animals*)
+consts appDomAnimal::"\<sigma>"   (*appropriation of domestic animals*)
 
-(*legal/world knowledge: meaning postulates for kinds of situations*)
-axiomatization where (* implicitly quantified for all situation/state/worlds *)
-W1: "\<lfloor>(appWildAnimal \<^bold>\<or> appDomesticAnimal) \<^bold>\<leftrightarrow> appAnimal\<rfloor>" and (*mutually exhaustive*)
-W2: "\<lfloor>appWildAnimal \<^bold>\<leftrightarrow> \<^bold>\<not>appDomesticAnimal\<rfloor>" and (*mutually exclusive*)
+(*LWK: meaning postulates for kinds of situations*)
+axiomatization where (*implic. quantified: all situations/worlds*)
+W1: "\<lfloor>(appWildAnimal \<^bold>\<or> appDomAnimal) \<^bold>\<leftrightarrow> appAnimal\<rfloor>" and 
+W2: "\<lfloor>appWildAnimal \<^bold>\<leftrightarrow> \<^bold>\<not>appDomAnimal\<rfloor>" and 
 W3: "\<lfloor>appWildAnimal \<^bold>\<rightarrow> appAnimal\<rfloor>"  and
-W4: "\<lfloor>appDomesticAnimal \<^bold>\<rightarrow> appAnimal\<rfloor>" and
+W4: "\<lfloor>appDomAnimal \<^bold>\<rightarrow> appAnimal\<rfloor>" and
 W5: "\<lfloor>appAnimal \<^bold>\<rightarrow> appObject\<rfloor>" 
-(* ... other situations regarding appropriation of objects, etc.*)
+(*... other situations regarding appropriation of objects, etc.*)
 
-(* legal corpus: value preferences conditioned on kinds of situations*)
-axiomatization where (*if situations not mutually exclusive: add ceteris paribus or make \<^bold>\<rightarrow> non-monotonic*)
-L1: "\<lfloor>appObject         \<^bold>\<rightarrow> (p\<upharpoonleft>STAB \<^bold>\<prec> d\<upharpoonleft>STAB)\<rfloor>" and (* legal certainty always on the side of defendant*)
+(*LWK: value preferences for kinds of situations*)
+axiomatization where 
+(* legal certainty always on the side of defendant*)
+L1: "\<lfloor>appObject         \<^bold>\<rightarrow> (p\<upharpoonleft>STAB \<^bold>\<prec> d\<upharpoonleft>STAB)\<rfloor>" and 
 L2: "\<lfloor>appAnimal         \<^bold>\<rightarrow> (x\<inverse>\<upharpoonleft>WILL \<^bold>\<prec> x\<upharpoonleft>[STAB\<oplus>GAIN])\<rfloor>" and 
 L3: "\<lfloor>appWildAnimal     \<^bold>\<rightarrow> (x\<inverse>\<upharpoonleft>WILL \<^bold>\<prec> x\<upharpoonleft>STAB)\<rfloor>" and        
-L4: "\<lfloor>appDomesticAnimal \<^bold>\<rightarrow> (x\<inverse>\<upharpoonleft>STAB \<^bold>\<prec> x\<upharpoonleft>[RELI\<oplus>RESP])\<rfloor>"
+L4: "\<lfloor>appDomAnimal \<^bold>\<rightarrow> (x\<inverse>\<upharpoonleft>STAB \<^bold>\<prec> x\<upharpoonleft>[RELI\<oplus>RESP])\<rfloor>"
 (* ... others*)
+(*remark: assumes that conditions are mutually exclusive;
+  we may want to add ceteris paribus or make \<^bold>\<rightarrow> non-monotonic*)
 
-lemma True nitpick[satisfy,card i=4] oops (*satisfiable, axioms are consistent*)
+lemma True nitpick[satisfy] oops (*satisfiable,model found*)
 
-(*legal/world knowledge: general legal notions (with an existing clear definition)*)
-consts Own::"c\<Rightarrow>\<sigma>"    (*object is owned by c (later related to RELI)*)
-consts Poss::"c\<Rightarrow>\<sigma>"   (*party c has actual possession of object (later related to STAB)*)
-consts Intent::"c\<Rightarrow>\<sigma>" (*party c has intention to possess the object (later related to WILL)*)
+(*LWK: general legal notions, with an existing clear definition*)
+consts Own::"c\<Rightarrow>\<sigma>"    (*object is owned by c*)
+consts Poss::"c\<Rightarrow>\<sigma>"   (*party c has actual possession of object*)
+consts Intent::"c\<Rightarrow>\<sigma>" (*party c has intention to possess object*)
+(*Remark: Own will later be related to RELI, Poss to STAB and
+  Intent to WILL*)
 
-(*legal/world knowledge: other general notions with some legal relevance (e.g. 'factors')*)
-consts Mtn::"c\<Rightarrow>\<sigma>" (*party c is responsible for the maintenance of the object (later related to RESP)*)
-consts Liv::"c\<Rightarrow>\<sigma>" (*party c is pursuing its livelihood (later related to GAIN)*)
+(*LWK: other notions with some legal relevance (e.g. 'factors')*)
+consts Liv::"c\<Rightarrow>\<sigma>" (*party c is pursuing its livelihood*)
+consts Mtn::"c\<Rightarrow>\<sigma>" (*c responsible for maintenance of object*)
+(*Remark: Mtn will be related to RESP and Liv to GAIN*)
 
 (*world knowledge: meaning postulates for general notions*)
 axiomatization where
@@ -43,19 +50,20 @@ axiomatization where
   W7: "\<lfloor>Own x \<^bold>\<rightarrow> (\<^bold>\<not>Own x\<inverse>)\<rfloor>"
 (* ... others*)
 
-(*legal corpus: value preferences conditioned on other notions (cf. 'factors')*)
+(*legal corpus: value prefs depend. on legal notions ('factors')*)
 axiomatization where
 L5: "\<lfloor>(Poss x \<^bold>\<and> \<^bold>\<not>Mtn x\<inverse>)  \<^bold>\<rightarrow> (x\<inverse>\<upharpoonleft>RELI \<^bold>\<prec> (x\<upharpoonleft>STAB))\<rfloor>"
 (* ... others*)
 
-(*legal corpus: (unconditional) value preferences given ruling decisions (cf. 'rules' based on 'factors') *)
+(*legal corpus: (unconditional) value preferences given ruling 
+  decisions ('rules' based on 'factors') *)
 axiomatization where (* relate factors to values*)
- R1: "\<lfloor>(Intent x \<^bold>\<and> For x) \<^bold>\<rightarrow> \<^bold>\<box>\<^sup>\<preceq>(x\<upharpoonleft>WILL)\<rfloor>" and  (*ruling for x given its intent to possess promotes WILL*)
+ R1: "\<lfloor>(Intent x \<^bold>\<and> For x) \<^bold>\<rightarrow> \<^bold>\<box>\<^sup>\<preceq>(x\<upharpoonleft>WILL)\<rfloor>" and  
+  (*R1: ruling for x given its intent to possess promotes WILL*)
  R2: "\<lfloor>(Liv x \<^bold>\<and> For x) \<^bold>\<rightarrow> \<^bold>\<box>\<^sup>\<preceq>(x\<upharpoonleft>GAIN)\<rfloor>" and
  R3: "\<lfloor>(Poss x \<^bold>\<and> For x) \<^bold>\<rightarrow> \<^bold>\<box>\<^sup>\<preceq>(x\<upharpoonleft>STAB)\<rfloor>" and
  R4: "\<lfloor>(Mtn x \<^bold>\<and> For x) \<^bold>\<rightarrow> \<^bold>\<box>\<^sup>\<preceq>(x\<upharpoonleft>RESP)\<rfloor>"
 (* ... others*)
 
-lemma True nitpick[satisfy] oops (*satisfiable, axioms are consistent*)
-
+lemma True nitpick[satisfy] oops (*satisfiable, consistent*)
 end
--- Preference-Logics/vanBenthemEtAl2009/WildAnimals2.thy
+++ Preference-Logics/vanBenthemEtAl2009/WildAnimals2.thy
@@ -1,61 +1,71 @@
-theory WildAnimals2 imports GeneralOntology2 (*Benzm.,Fuenmayor & Lomfeld, 2020*)               
+theory WildAnimals2     (*Benzmüller, Fuenmayor & Lomfeld, 2020*)  
+  imports GeneralOntology2 
 begin
 
 typedecl e  (*entities*)
-(* define case-specific 'world- vocabulary'*)
+
+(*case-specific 'world-vocabulary'*)
 consts Animal::"e\<Rightarrow>\<sigma>" 
 consts Domestic::"e\<Rightarrow>\<sigma>" 
 consts Fox::"e\<Rightarrow>\<sigma>" 
 consts Pet::"e\<Rightarrow>\<sigma>" 
 consts Parrot::"e\<Rightarrow>\<sigma>" 
 consts Captures::"c\<Rightarrow>e\<Rightarrow>\<sigma>" 
-consts \<alpha>::"e" (* appropriated animal (the fox, parrot, whale, etc. in question)*)
+consts \<alpha>::"e" (*appropriated animal (fox, parrot, whale, etc.*)
 
-axiomatization where (*meaning postulates for the case-specific notions above*)
+(*meaning postulates for the case-specific notions above*)
+axiomatization where 
 CW1: "\<lfloor>\<^bold>\<forall>x. Parrot x \<^bold>\<rightarrow> Domestic x\<rfloor>" and 
 CW2: "\<lfloor>\<^bold>\<forall>x. (Fox x) \<^bold>\<and> (Pet x) \<^bold>\<rightarrow> (Domestic x)\<rfloor>" and
 CW3: "\<lfloor>(Fox \<alpha>) \<^bold>\<and> (\<^bold>\<exists>c. Captures c \<alpha>) \<^bold>\<rightarrow> appWildAnimal\<rfloor>" and
-CW4: "\<lfloor>(Domestic \<alpha>) \<^bold>\<and> (\<^bold>\<exists>c. Captures c \<alpha>) \<^bold>\<rightarrow> appDomesticAnimal\<rfloor>" 
+CW4: "\<lfloor>(Domestic \<alpha>) \<^bold>\<and> (\<^bold>\<exists>c. Captures c \<alpha>) \<^bold>\<rightarrow> appDomAnimal\<rfloor>" 
 (* ... others*)
 
-lemma True nitpick[satisfy,card i=4] oops (*satisfiable, axioms are consistent*)
+lemma True nitpick[satisfy,card i=4] oops (*satisfiable*)
 
 (*case-specific notions, cf. legal 'factors'*)
-consts Pursues::"c\<Rightarrow>e\<Rightarrow>\<sigma>" (*c is pursuing an animal (later related to WILL)*)
-consts OwnLand::"c\<Rightarrow>\<sigma>" (*event takes place on c's own land (later related to RELI)*)
+consts Pursues::"c\<Rightarrow>e\<Rightarrow>\<sigma>" (*c is pursuing an animal*)
+consts OwnLand::"c\<Rightarrow>\<sigma>"    (*event takes place on c's own land *)
+(*remark: Pursues will later be related to WILL, OwnLand to RELI*)
 
 (*meaning postulates for some legal 'factors'*)
 axiomatization where
  CW5: "\<lfloor>(Pursues x \<alpha>) \<^bold>\<rightarrow> (Intent x)\<rfloor>"
 (* ... others*)
 
-(*case-specific legal corpus, e.g. (unconditional) value preferences given ruling decisions*)
+(*case-specific legal corpus, e.g. (unconditional) value 
+  preferences given ruling decisions*)
 axiomatization where
  CR2: "\<lfloor>(OwnLand x \<^bold>\<and> For x) \<^bold>\<rightarrow> \<^bold>\<box>\<^sup>\<preceq>(x\<upharpoonleft>RELI)\<rfloor>"
 (* ... others*)
 
 (*explore possible inferences (implicit knowledge)*)
-lemma "\<exists>c. \<lfloor>((Fox \<alpha>) \<^bold>\<and> (Captures c \<alpha>)) \<^bold>\<rightarrow> (x\<inverse>\<upharpoonleft>WILL \<^bold>\<prec> x\<upharpoonleft>STAB)\<rfloor>" using CW3 L3 by blast
-lemma "\<lfloor>appDomesticAnimal \<^bold>\<rightarrow> (x\<inverse>\<upharpoonleft>WILL \<^bold>\<prec> x\<upharpoonleft>STAB)\<rfloor>" nitpick[satisfy] nitpick oops (*contingent*) 
-lemma "\<not>\<lfloor>(Fox \<alpha>) \<^bold>\<and> (\<^bold>\<exists>c. Captures c \<alpha>) \<^bold>\<and> (Pet \<alpha>)\<rfloor>" using CW2 CW3 CW4 W2 by blast (*inconsistent SoA*) 
+lemma "\<exists>c. \<lfloor>((Fox \<alpha>) \<^bold>\<and> (Captures c \<alpha>))
+            \<^bold>\<rightarrow> (x\<inverse>\<upharpoonleft>WILL \<^bold>\<prec> x\<upharpoonleft>STAB)\<rfloor>" using CW3 L3 by blast
+lemma "\<lfloor>appDomAnimal \<^bold>\<rightarrow> (x\<inverse>\<upharpoonleft>WILL \<^bold>\<prec> x\<upharpoonleft>STAB)\<rfloor>" 
+  nitpick[satisfy] nitpick oops (*contingent*) 
+lemma "\<not>\<lfloor>(Fox \<alpha>) \<^bold>\<and> (\<^bold>\<exists>c. Captures c \<alpha>) \<^bold>\<and> (Pet \<alpha>)\<rfloor>" 
+  using CW2 CW3 CW4 W2 by blast (*inconsistent SoA*) 
 
 (******** TODO: experimenting with cases ************)
 (* axiomatization where CONSISTENT_p:"\<lfloor>\<^bold>\<not>INCONS p\<rfloor>" and CONSISTENT_d:"\<lfloor>\<^bold>\<not>INCONS d\<rfloor>" *)
-lemma "\<not>(\<exists>(x::i) y z. \<not>(x = y) \<and> \<not>(x = z) \<and> \<not>(y = z))" nitpick oops (*more than 2 worlds?*)
+lemma "\<not>(\<exists>(x::i) y z. x \<noteq> y \<and> x \<noteq> z \<and> y \<noteq> z)" 
+  nitpick oops (*more than 2 worlds?*)
 lemma "True" nitpick[satisfy,card i=4] oops
 
 (*Pierson v. Post*)         
-lemma "\<lfloor>(Fox \<alpha>) \<^bold>\<and> (Pursues p \<alpha>) \<^bold>\<and> (\<^bold>\<not>Pursues d \<alpha>) \<^bold>\<and> (Poss d) \<^bold>\<and> For d\<rfloor>" 
-  nitpick[satisfy,show_all,card i=4]  oops (*non-trivial models exist*)
+lemma "\<lfloor>Fox \<alpha> \<^bold>\<and> Pursues p \<alpha> \<^bold>\<and> (\<^bold>\<not>Pursues d \<alpha>) \<^bold>\<and> Poss d \<^bold>\<and> For d\<rfloor>" 
+  nitpick[satisfy,show_all,card i=4] oops (*non-trivial model*)
 
-lemma "\<lfloor>(Fox \<alpha>) \<^bold>\<and> (Pursues p \<alpha>) \<^bold>\<and> (\<^bold>\<not>Pursues d \<alpha>) \<^bold>\<and> (Poss d) \<^bold>\<rightarrow> For d\<rfloor>" 
+lemma "\<lfloor>Fox \<alpha> \<^bold>\<and> Pursues p \<alpha> \<^bold>\<and> (\<^bold>\<not>Pursues d \<alpha>) \<^bold>\<and> Poss d \<^bold>\<rightarrow> For d\<rfloor>" 
   nitpick[show_all,card i=2] oops (*countermodel found*)
 
 (*Conti v. ASPCA*)
-lemma "\<lfloor>(Parrot \<alpha>) \<^bold>\<and> (Captures d \<alpha>) \<^bold>\<and> (Mtn p) \<^bold>\<and> (Own p) \<^bold>\<and> (Poss d) \<^bold>\<and> For p\<rfloor>"
-  nitpick[satisfy,show_all,card i=4]  oops (*non-trivial models exist*)
+lemma 
+  "\<lfloor>Parrot \<alpha> \<^bold>\<and> Captures d \<alpha> \<^bold>\<and> Mtn p \<^bold>\<and> Own p \<^bold>\<and> Poss d \<^bold>\<and> For p\<rfloor>"
+  nitpick[satisfy,show_all,card i=4] oops (*non-trivial model*)
 
-lemma "\<lfloor>(Parrot \<alpha>) \<^bold>\<and> (Captures d \<alpha>) \<^bold>\<and> (Mtn p) \<^bold>\<and> (Own p) \<^bold>\<and> (Poss d) \<^bold>\<rightarrow> For p\<rfloor>"
+lemma 
+  "\<lfloor>Parrot \<alpha> \<^bold>\<and> Captures d \<alpha> \<^bold>\<and> Mtn p \<^bold>\<and> Own p \<^bold>\<and> Poss d \<^bold>\<rightarrow> For p\<rfloor>"
   nitpick[show_all,card i=4] oops (*countermodel found*)
-
 end
