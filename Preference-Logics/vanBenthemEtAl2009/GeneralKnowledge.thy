9dc744653bd305983e4a83b3dc001e3e5d56fbac
Benzmüller <cbenzmueller@ontomax.campus.fu-berlin.de>
2020-06-15 12:01:59 +0200

    update

--- Preference-Logics/vanBenthemEtAl2009/GeneralKnowledge.thy
+++ Preference-Logics/vanBenthemEtAl2009/GeneralKnowledge.thy
@@ -19,46 +19,41 @@ W5: "\<lfloor>appAnimal \<^bold>\<rightarrow> appObject\<rfloor>"
 
 (*LWK: value preferences for kinds of situations*)
 axiomatization where 
-L1: "\<lfloor>appObject     \<^bold>\<rightarrow> (p\<upharpoonleft>STAB  \<prec>\<^sub>A\<^sub>A d\<upharpoonleft>STAB)\<rfloor>" and 
-L2: "\<lfloor>appAnimal     \<^bold>\<rightarrow> (x\<inverse>\<upharpoonleft>WILL \<prec>\<^sub>A\<^sub>A x\<upharpoonleft>[STAB\<oplus>GAIN])\<rfloor>" and 
-L3: "\<lfloor>appWildAnimal \<^bold>\<rightarrow> (x\<inverse>\<upharpoonleft>WILL \<prec>\<^sub>A\<^sub>A x\<upharpoonleft>STAB)\<rfloor>" and        
-L4: "\<lfloor>appDomAnimal  \<^bold>\<rightarrow> (x\<inverse>\<upharpoonleft>STAB \<prec>\<^sub>A\<^sub>A x\<upharpoonleft>[RELI\<oplus>RESP])\<rfloor>"
+L1: "\<lfloor>appAnimal     \<^bold>\<rightarrow> (p\<upharpoonleft>STAB  \<prec>\<^sub>A\<^sub>A d\<upharpoonleft>STAB)\<rfloor>" and 
+L2: "\<lfloor>appWildAnimal \<^bold>\<rightarrow> (x\<inverse>\<upharpoonleft>WILL \<prec>\<^sub>A\<^sub>A x\<upharpoonleft>STAB)\<rfloor>" and        
+L3: "\<lfloor>appDomAnimal  \<^bold>\<rightarrow> (x\<inverse>\<upharpoonleft>STAB \<prec>\<^sub>A\<^sub>A x\<upharpoonleft>[RELI\<oplus>RESP])\<rfloor>"
 (*\<dots>further preferences\<dots>*)
 
-lemma True nitpick[satisfy,show_all,eval=WILL] oops (*consistency, model found*)
+lemma True nitpick[satisfy] oops (*consistency, model found*)
 
 (*LWK: general legal notions, with an existing clear definition*)
-consts Own::"c\<Rightarrow>\<sigma>"    (*object is owned by c*)
+consts Own::"c\<Rightarrow>\<sigma>"    (*object is owned by party c*)
 consts Poss::"c\<Rightarrow>\<sigma>"   (*party c has actual possession of object*)
 consts Intent::"c\<Rightarrow>\<sigma>" (*party c has intention to possess object*)
-(*Remark: Own will later be related to RELI, Poss to STAB and
-  Intent to WILL*)
 
 (*LWK: other notions with some legal relevance (e.g. 'factors')*)
 consts Liv::"c\<Rightarrow>\<sigma>" (*party c is pursuing its livelihood*)
-consts Mtn::"c\<Rightarrow>\<sigma>" (*c responsible for maintenance of object*)
-(*Remark: Mtn will be related to RESP and Liv to GAIN*)
+consts Mtn::"c\<Rightarrow>\<sigma>" (*party c responsible for maintenance of object*)
 
-(*world knowledge: meaning postulates for general notions*)
+(*LWK: meaning postulates for general notions*)
 axiomatization where
-  W6: "\<lfloor>Poss x \<^bold>\<rightarrow> (\<^bold>\<not>Poss x\<inverse>)\<rfloor>" and 
-  W7: "\<lfloor>Own x \<^bold>\<rightarrow> (\<^bold>\<not>Own x\<inverse>)\<rfloor>"
-(* ... others*)
+W6: "\<lfloor>Poss x \<^bold>\<rightarrow> (\<^bold>\<not>Poss x\<inverse>)\<rfloor>" and 
+W7: "\<lfloor>Own x \<^bold>\<rightarrow> (\<^bold>\<not>Own x\<inverse>)\<rfloor>"
+(*\<dots>others\<dots>*)
 
-(*legal corpus: value prefs depend. on legal notions ('factors')*)
+(*LWK: conditional legal value preferences*)
 axiomatization where
 L5: "\<lfloor>(Poss x \<^bold>\<and> \<^bold>\<not>Mtn x\<inverse>)  \<^bold>\<rightarrow> (x\<inverse>\<upharpoonleft>RELI \<^bold>\<prec>\<^sub>A\<^sub>A (x\<upharpoonleft>STAB))\<rfloor>"
-(* ... others*)
+(*\<dots>others\<dots>*)
 
-(*legal corpus: (unconditional) value preferences given ruling 
-  decisions ('rules' based on 'factors') *)
-axiomatization where (* relate factors to values*)
- R1: "\<lfloor>(Intent x \<^bold>\<and> For x) \<^bold>\<rightarrow> \<^bold>\<box>\<^sup>\<preceq>(x\<upharpoonleft>WILL)\<rfloor>" and  
-  (*R1: ruling for x given its intent to possess promotes WILL*)
- R2: "\<lfloor>(Liv x \<^bold>\<and> For x) \<^bold>\<rightarrow> \<^bold>\<box>\<^sup>\<preceq>(x\<upharpoonleft>GAIN)\<rfloor>" and
- R3: "\<lfloor>(Poss x \<^bold>\<and> For x) \<^bold>\<rightarrow> \<^bold>\<box>\<^sup>\<preceq>(x\<upharpoonleft>STAB)\<rfloor>" and
- R4: "\<lfloor>(Mtn x \<^bold>\<and> For x) \<^bold>\<rightarrow> \<^bold>\<box>\<^sup>\<preceq>(x\<upharpoonleft>RESP)\<rfloor>"
-(* ... others*)
 
-lemma True nitpick[satisfy] oops (*satisfiable, consistent*)
+(*LWK: value preferences given certain situational factors*)
+axiomatization where 
+R1: "\<lfloor>(Intent x \<^bold>\<and> For x) \<^bold>\<rightarrow> \<^bold>\<box>\<^sup>\<preceq>(x\<upharpoonleft>WILL)\<rfloor>" and  
+R2: "\<lfloor>(Liv x \<^bold>\<and> For x) \<^bold>\<rightarrow> \<^bold>\<box>\<^sup>\<preceq>(x\<upharpoonleft>GAIN)\<rfloor>" and
+R3: "\<lfloor>(Poss x \<^bold>\<and> For x) \<^bold>\<rightarrow> \<^bold>\<box>\<^sup>\<preceq>(x\<upharpoonleft>STAB)\<rfloor>" and
+R4: "\<lfloor>(Mtn x \<^bold>\<and> For x) \<^bold>\<rightarrow> \<^bold>\<box>\<^sup>\<preceq>(x\<upharpoonleft>RESP)\<rfloor>"
+(*\<dots>others\<dots>*)
+
+lemma True nitpick[satisfy] oops (*consistency, model found*)
 end
