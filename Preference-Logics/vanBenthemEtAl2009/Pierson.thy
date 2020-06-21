04c03d556f8614fa937ac9d7a086b621d7ae7d13
davfuenmayor <davfuenmayor@gmail.com>
2020-06-21 16:23:12 +0200

    update case studies

--- Preference-Logics/vanBenthemEtAl2009/Pierson.thy
+++ Preference-Logics/vanBenthemEtAl2009/Pierson.thy
@@ -2,9 +2,8 @@ theory Pierson    (*** Benzmüller, Fuenmayor & Lomfeld, 2020 ***)
   imports GeneralKnowledge
 begin (*** Pierson v. Post "wild animal" case **)
 
-consts \<alpha>::"e" (*appropriated animal (fox in this case) *)
-
 (*case-specific 'world-vocabulary'*)
+consts \<alpha>::"e" (*appropriated animal (fox in this case) *)
 consts Pursue::"c\<Rightarrow>e\<Rightarrow>\<sigma>"
 consts Capture::"c\<Rightarrow>e\<Rightarrow>\<sigma>"
 
@@ -21,19 +20,20 @@ abbreviation "Pierson_facts \<equiv> \<lfloor>Fox \<alpha> \<^bold>\<and> (FreeR
   (\<^bold>\<not>Pet \<alpha>) \<^bold>\<and> Pursue p \<alpha> \<^bold>\<and> (\<^bold>\<not>Pursue d \<alpha>) \<^bold>\<and> Capture d \<alpha>\<rfloor>"
 
 (*decision for defendant (Pierson) is compatible with premises*)
-lemma "Pierson_facts \<and> \<lfloor>\<^bold>\<not>INCONS\<^sup>d\<rfloor> \<and> \<lfloor>For p \<^bold>\<prec> For d\<rfloor>"
+lemma "Pierson_facts \<and> \<lfloor>\<^bold>\<not>INCONS\<^sup>p\<rfloor> \<and> \<lfloor>\<^bold>\<not>INCONS\<^sup>d\<rfloor> \<and> \<lfloor>For p \<^bold>\<prec> For d\<rfloor>"
   nitpick[satisfy,card i=4] oops (* (non-trivial) model found*)
 
 (*decision for plaintiff (Post) is compatible with premises*)
-lemma "Pierson_facts \<and> \<lfloor>\<^bold>\<not>INCONS\<^sup>p\<rfloor> \<and> \<lfloor>For d \<^bold>\<prec> For p\<rfloor>"
+lemma "Pierson_facts \<and> \<lfloor>\<^bold>\<not>INCONS\<^sup>p\<rfloor> \<and> \<lfloor>\<^bold>\<not>INCONS\<^sup>d\<rfloor> \<and> \<lfloor>For d \<^bold>\<prec> For p\<rfloor>"
   nitpick[satisfy,card i=4] oops (* (non-trivial) model found*)
 
 (*decision for defendant (Pierson) is provable*)
-lemma assumes Pierson_facts shows "\<lfloor>For p \<^bold>\<prec> For d\<rfloor>"
- by (metis assms CW1 CW2 W6 W8 ForAx L2 R1 other.simps(2) rBR)
+theorem assumes Pierson_facts shows "\<lfloor>For p \<^bold>\<prec> For d\<rfloor>"
+  by (metis assms CW1 CW2 W6 W8 ForAx L2 R1 other.simps(2) rBR)
+
 (*while a decision for the plaintiff is not*)
 lemma assumes Pierson_facts shows "\<lfloor>For d \<^bold>\<prec> For p\<rfloor>"
- nitpick[card i=4] oops (*counterexample found*)
+  nitpick[card i=4] oops (*counterexample found*)
 
 (****************** pro-Post's argument ****************)
 (* Theory amendment: the animal is not free-roaming since it
@@ -46,19 +46,18 @@ abbreviation "Post_facts \<equiv> \<lfloor>Fox \<alpha> \<^bold>\<and> (\<^bold>
    Hunter p \<^bold>\<and> Pursue p \<alpha> \<^bold>\<and> (\<^bold>\<not>Pursue d \<alpha>) \<^bold>\<and> Capture d \<alpha>\<rfloor>"
 
 (*decision for defendant (Pierson) is compatible with premises*)
-lemma "Post_facts \<and> \<lfloor>\<^bold>\<not>INCONS\<^sup>d\<rfloor> \<and> \<lfloor>For p \<^bold>\<prec> For d\<rfloor>"
+lemma "Post_facts \<and> \<lfloor>\<^bold>\<not>INCONS\<^sup>p\<rfloor> \<and> \<lfloor>\<^bold>\<not>INCONS\<^sup>d\<rfloor> \<and> \<lfloor>For p \<^bold>\<prec> For d\<rfloor>"
   nitpick[satisfy,card i=4] oops (* (non-trivial) model found*)
 
 (*decision for plaintiff (Post) is compatible with premises too*)
-lemma "Post_facts \<and> \<lfloor>\<^bold>\<not>INCONS\<^sup>p\<rfloor> \<and> \<lfloor>For d \<^bold>\<prec> For p\<rfloor>"
+lemma "Post_facts \<and> \<lfloor>\<^bold>\<not>INCONS\<^sup>p\<rfloor> \<and> \<lfloor>\<^bold>\<not>INCONS\<^sup>d\<rfloor> \<and> \<lfloor>For d \<^bold>\<prec> For p\<rfloor>"
   nitpick[satisfy,card i=4] oops (* (non-trivial) model found*)
 
 (*indeed, a decision for plaintiff (Post) now becomes provable*)
-lemma assumes Post_facts shows "\<lfloor>For d \<^bold>\<prec> For p\<rfloor>"
+theorem assumes Post_facts shows "\<lfloor>For d \<^bold>\<prec> For p\<rfloor>"
   using assms by (metis CW3 ForAx CL1 R3 other.simps rBR)
+
 (*while a decision for the defendant is now refutable*)
 lemma assumes Post_facts shows "\<lfloor>For p \<^bold>\<prec> For d\<rfloor>" 
   nitpick[card i=4] oops (* counterexample found*)
 end
-
-
