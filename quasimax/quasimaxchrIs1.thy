2ded3c4d5f6867c7fc8444baedac24eec9fbc7cd
xavierparent <37213722+xavierparent@users.noreply.github.com>
2021-09-19 10:57:01 +0200

    update quasi max
    update quasimax

--- quasimax/quasimaxchrIs1.thy
+++ quasimax/quasimaxchrIs1.thy
@@ -1,7 +1,17 @@
-theory E imports Main       (* Aqvist's System E: C. Benzmüller & X. Parent, 2019 *)
-begin      nitpick_params [sat_solver = MiniSat_JNI]
-typedecl i (*Possible worlds.*) type_synonym \<sigma> = "(i\<Rightarrow>bool)" 
-consts aw::i (*Actual world.*)  
+theory quasimax imports Main       (* Quasi-maximality: C. Benzmüller & X. Parent, 2020 *)
+
+(* Quasi-maximality, Deb 1977, means maximal wrt transitive closure of the betterness relation restricted to the menu *)
+(* Quasi-maximality: ideally the transitive closure should be paramatrized by a context.
+It is restricted to the set of antecedent-worlds. The transitive closure should be recalculated each time
+each time the set of worlds in which the antecedent is true changes  *)
+
+begin
+typedecl i (*Possible worlds.*) 
+type_synonym \<sigma> = "(i\<Rightarrow>bool)" 
+type_synonym \<alpha> = "i\<Rightarrow>\<sigma>" (*Type of betterness relation between worlds.*)
+type_synonym \<tau> = "\<sigma>\<Rightarrow>\<sigma>"
+
+consts aw::i (*Actual world.*)
 abbreviation etrue  :: "\<sigma>" ("\<^bold>\<top>") where "\<^bold>\<top> \<equiv> \<lambda>w. True" 
 abbreviation efalse :: "\<sigma>" ("\<^bold>\<bottom>")  where "\<^bold>\<bottom> \<equiv> \<lambda>w. False"   
 abbreviation enot :: "\<sigma>\<Rightarrow>\<sigma>" ("\<^bold>\<not>_"[52]53)  where "\<^bold>\<not>\<phi> \<equiv> \<lambda>w. \<not>\<phi>(w)" 
@@ -17,15 +27,33 @@ abbreviation eexists ("\<^bold>\<exists>") where "\<^bold>\<exists>\<Phi> \<equi
 abbreviation eexistsB (binder"\<^bold>\<exists>"[8]9) where "\<^bold>\<exists>x. \<phi>(x) \<equiv> \<^bold>\<exists>\<phi>" 
 
 abbreviation ebox :: "\<sigma>\<Rightarrow>\<sigma>" ("\<box>") where "\<box> \<equiv> \<lambda>\<phi> w.  \<forall>v. \<phi>(v)"  
-consts R :: "i\<Rightarrow>\<sigma>" (infixr "R" 70) (*Betterness relation, cf. def. of \<circle><_|_>.*) 
-abbreviation eopt  :: "\<sigma>\<Rightarrow>\<sigma>" ("opt<_>") 
-  where "opt<\<phi>> \<equiv> (\<lambda>v. ( (\<phi>)(v) \<and> (\<forall>x. ((\<phi>)(x)  \<longrightarrow>  v R x) )) )" 
-abbreviation esubset :: "\<sigma>\<Rightarrow>\<sigma>\<Rightarrow>bool" (infix "\<^bold>\<subseteq>" 53)
+
+
+(* Some useful relations for constraining accessibility relations*)
+
+
+definition transitive :: "\<alpha>\<Rightarrow>bool" where "transitive R \<equiv> \<forall>x y z. R x y \<and> R y z \<longrightarrow> R x z"
+definition sub_rel :: "\<alpha>\<Rightarrow>\<alpha>\<Rightarrow>bool" where "sub_rel R Q \<equiv> \<forall>u v. R u v \<longrightarrow> Q u v"
+
+(*In HOL the transitive closure of a relation can be defined in a single line.*)
+definition tc :: "\<alpha>\<Rightarrow>\<alpha>" where "tc R \<equiv> \<lambda>x y.\<forall>Q. transitive Q \<longrightarrow> (sub_rel R Q \<longrightarrow> Q x y)"
+
+consts BRel :: "\<alpha>" (infixr "R" 70) (*Betterness relation, cf. def. of \<circle><_|_>.*)
+(*Original Max, wrt to R
+
+abbreviation max  :: "\<sigma>\<Rightarrow>\<sigma>" ("max<_>")
+  where "max<\<phi>> \<equiv> (\<lambda>v. ( (\<phi>)(v) \<and> (\<forall>x. ((\<phi>)(x)\<and> x R v  \<longrightarrow>  v  R x))) )" 
+*)
+
+abbreviation qmax  :: "\<sigma>\<Rightarrow>\<sigma>" ("qmax<_>")(*quasimax, i.e. max wrt to tc(R)*)
+  where "qmax<\<phi>> \<equiv> (\<lambda>v. ((\<phi> v) \<and> (\<forall>x. ((\<phi> x) \<and> ((tc BRel) x v)  \<longrightarrow>  ((tc BRel) v x)))) )" 
+
+abbreviation subset :: "\<sigma>\<Rightarrow>\<sigma>\<Rightarrow>bool" (infix "\<^bold>\<subseteq>" 53)
   where "\<phi> \<^bold>\<subseteq> \<psi> \<equiv> \<forall>x. \<phi> x \<longrightarrow> \<psi> x"
-abbreviation econd  :: "\<sigma>\<Rightarrow>\<sigma>\<Rightarrow>\<sigma>" ("\<circle><_|_>")
-  where "\<circle><\<psi>|\<phi>> \<equiv>  \<lambda>w. opt<\<phi>> \<^bold>\<subseteq> \<psi>"
-abbreviation euncobl :: "\<sigma>\<Rightarrow>\<sigma>" ("\<^bold>\<circle><_>")   
-  where "\<^bold>\<circle><\<phi>> \<equiv> \<circle><\<phi>|\<^bold>\<top>>" 
+
+abbreviation cond  :: "\<sigma>\<Rightarrow>\<sigma>\<Rightarrow>\<sigma>" ("\<circle><_|_>")
+  where "\<circle><\<psi>|\<phi>> \<equiv>  \<lambda>w. qmax<\<phi>> \<^bold>\<subseteq> \<psi>"
+
 
 abbreviation evalid :: "\<sigma>\<Rightarrow>bool" ("\<lfloor>_\<rfloor>"[8]109)  (*Global validity.*)
   where "\<lfloor>p\<rfloor> \<equiv> \<forall>w. p w"
@@ -34,14 +62,14 @@ abbreviation ecjactual :: "\<sigma>\<Rightarrow>bool" ("\<lfloor>_\<rfloor>\<^su
 
 lemma True nitpick [satisfy,user_axioms,expect=genuine] oops (*Consistency conf.*)
 
-(*Correspondence theory.*)
+(*Correspondence theory. old stuff
 lemma assumes "\<forall>x y z. x R y \<and> y R z \<longrightarrow> x R z" 
-  shows "\<lfloor>((\<circle><\<psi>|\<phi>>) \<^bold>\<and> \<^bold>\<not>(\<circle><\<^bold>\<not>\<xi>|\<phi>>)) \<^bold>\<rightarrow> \<circle><\<psi>|\<phi>\<^bold>\<and>\<xi>>\<rfloor>" 
-  sledgehammer oops 
+      shows "\<lfloor>((\<circle><\<psi>|\<phi>>) \<^bold>\<and> \<^bold>\<not>(\<circle><\<^bold>\<not>\<xi>|\<phi>>)) \<^bold>\<rightarrow> \<circle><\<psi>|\<phi>\<^bold>\<and>\<xi>>\<rfloor>" 
+  using assms by blast
 
 lemma assumes "\<lfloor>((\<circle><\<psi>|\<phi>>) \<^bold>\<and> \<^bold>\<not>(\<circle><\<^bold>\<not>\<xi>|\<phi>>)) \<^bold>\<rightarrow> \<circle><\<psi>|\<phi>\<^bold>\<and>\<xi>>\<rfloor>" 
       shows "\<forall>x y z. x R y \<and> y R z \<longrightarrow> x R z" 
-  nitpick [show_all,format=2]  (*Countermodel presented by Nitpick.*)
-
+  nitpick [show_all,format=2] oops (*Countermodel presented by Nitpick.*)
+*)
 end
 
