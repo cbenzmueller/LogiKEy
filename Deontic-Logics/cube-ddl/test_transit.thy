53e0f84e27b840652e94e53950a46e5bad414dad
Benzmüller <cbenzmueller@ontomax.imp.fu-berlin.de>
2022-12-19 10:49:56 +0100

    update

--- Deontic-Logics/cube-ddl/test_transit.thy
+++ Deontic-Logics/cube-ddl/test_transit.thy
@@ -4,12 +4,12 @@ imports cube
 
 begin
 
-lemma assumes Sp: "\<lfloor>( \<^bold>\<not>\<odot><\<^bold>\<not>\<psi>|\<phi>> \<^bold>\<and> \<odot><(\<psi>\<^bold>\<rightarrow>\<chi>)|\<phi>>) \<^bold>\<rightarrow> \<odot><\<chi>|(\<phi>\<^bold>\<and>\<psi>)>\<rfloor>"
-      assumes Trans: "\<lfloor>(\<^bold>\<not>\<odot><\<^bold>\<not>\<phi>|(\<phi>\<^bold>\<or>\<psi>)> \<^bold>\<and> \<^bold>\<not>\<odot><\<^bold>\<not>\<psi>|(\<psi>\<^bold>\<or>\<xi>)>)\<^bold>\<rightarrow>\<^bold>\<not>\<odot><\<^bold>\<not>\<phi>|(\<phi>\<^bold>\<or>\<xi>)>\<rfloor>"
-      shows wDR: "\<lfloor> \<odot><\<^bold>\<not>\<psi>|(\<phi>\<^bold>\<or>\<psi>)> \<^bold>\<rightarrow> (\<odot><\<^bold>\<not>\<psi>|\<psi>> \<^bold>\<or> \<odot><\<^bold>\<not>\<psi>|\<phi>>)\<rfloor>"     
-  sledgehammer (* unprovable*) oops
-  nitpick (* time out*) 
-  
+lemma 
+  assumes Sp: "\<lfloor>( \<^bold>\<not>\<odot><\<^bold>\<not>\<psi>|\<phi>> \<^bold>\<and> \<odot><(\<psi>\<^bold>\<rightarrow>\<chi>)|\<phi>>) \<^bold>\<rightarrow> \<odot><\<chi>|(\<phi>\<^bold>\<and>\<psi>)>\<rfloor>"
+      and Trans: "\<lfloor>(\<^bold>\<not>\<odot><\<^bold>\<not>\<phi>|(\<phi>\<^bold>\<or>\<psi>)> \<^bold>\<and> \<^bold>\<not>\<odot><\<^bold>\<not>\<psi>|(\<psi>\<^bold>\<or>\<xi>)>)\<^bold>\<rightarrow>\<^bold>\<not>\<odot><\<^bold>\<not>\<phi>|(\<phi>\<^bold>\<or>\<xi>)>\<rfloor>"
+  shows wDR: "\<lfloor> \<odot><\<^bold>\<not>\<psi>|(\<phi>\<^bold>\<or>\<psi>)> \<^bold>\<rightarrow> (\<odot><\<^bold>\<not>\<psi>|\<psi>> \<^bold>\<or> \<odot><\<^bold>\<not>\<psi>|\<phi>>)\<rfloor>"     
+  nitpick[card=3] (* counterexample found *) 
+  oops
 
 
 end
