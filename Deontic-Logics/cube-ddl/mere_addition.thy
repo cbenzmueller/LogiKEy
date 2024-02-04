3b6c8b19dc528073812b186f0471bcd53dd74ec2
xavierparent <37213722+xavierparent@users.noreply.github.com>
2024-02-04 12:58:09 +0100

    update
    update

--- Deontic-Logics/cube-ddl/mere_addition.thy
+++ Deontic-Logics/cube-ddl/mere_addition.thy
@@ -121,9 +121,6 @@ theorem QuasiInconsMax:
   sledgehammer
   oops
 
-
-
-
 (* Sledgehammer shows consistency if transitivity is weakened into 
 quasi-tran*)
 
@@ -207,6 +204,83 @@ theorem SuzuInConsLewis:
   shows false
   sledgehammer
   oops
+
+(*** With the Kratzer rule  ***)
+
+axiomatization where
+(* A is striclty better than B*)
+ PPPP0: "\<lfloor>(\<^bold>\<not>\<ominus><\<^bold>\<not>a|a\<^bold>\<or>b>\<^bold>\<and>\<ominus><\<^bold>\<not>b|a\<^bold>\<or>b>)\<rfloor>" and
+(* Aplus is at least as good as A*)
+ PPPP1: "\<lfloor>\<^bold>\<not>\<ominus><\<^bold>\<not>aplus|a\<^bold>\<or>aplus>\<rfloor>" and
+(* B is strictly better than Aplus*)
+ PPPP2: "\<lfloor>(\<^bold>\<not>\<ominus><\<^bold>\<not>b|aplus\<^bold>\<or>b> \<^bold>\<and> \<ominus><\<^bold>\<not>aplus|aplus\<^bold>\<or>b>)\<rfloor>"
+
+(* Sledgehammer finds PP0-PP2 inconsistent given 
+transitivity of the betterness relation in the models*)
+
+theorem TransInconsK:
+  assumes transitivity
+  shows False 
+  sledgehammer
+  oops
+  
+(* Nitpick shows consistency in the absence of transitivity*)
+
+lemma true
+  nitpick [satisfy, card i=3]   (*model found*)
+  oops
+
+(* Sledgehammer confirms inconsistency in the presence of the interval order condition*)
+
+theorem ioInconsK:
+  assumes reflexivity Ferrers
+  shows false
+  sledgehammer 
+  by (metis P0 P1 P2 assms(2))
+  
+(* Nitpick shows consistency if transitivity is weakened into acyclicity or quasi-transitivity*)
+
+theorem AcyclconsK:
+  assumes loopfree
+  shows true
+  nitpick [show_all,satisfy,card=3] (* model found for card i=3 *) 
+  oops
+
+theorem QuasiconsK:
+  assumes Quasitransit
+  shows true
+  nitpick [show_all,satisfy] 
+  oops
+
+theorem QuasiInconsK':
+  assumes Quasitransit
+  shows false
+  sledgehammer
+  oops
+
+
+(* Sledgehammer shows consistency if transitivity is weakened into 
+quasi-tran*)
+
+theorem SuzuConsK:
+  assumes Suzumura
+  shows true
+  nitpick [show_all,satisfy,card=4]
+  oops
+
+theorem SuzuInConsK:
+  assumes Suzumura
+  shows false
+  sledgehammer
+  oops
+
+
+
+
+
+
+
+
 
 
 end
