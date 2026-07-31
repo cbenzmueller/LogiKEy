8ab87e13b1fd2e43c7ed03deefdfda7816249065
lucapasetto <148212465+lucapasetto@users.noreply.github.com>
2026-07-31 16:24:14 +0200

    Update LMP_case_study.thy

--- 2026-AAMAS-Data/LMP_case_study.thy
+++ 2026-AAMAS-Data/LMP_case_study.thy
@@ -1,3 +1,8 @@
+(* 
+Luca Pasetto, Christoph Benzmüller, and Réka Markovich. 2026.
+Formalizing Mental Privacy in LogiKEy.
+*)
+
 section \<open>Right to Mental Privacy in HOL: Case Study\<close>
 
 theory LMP_case_study
