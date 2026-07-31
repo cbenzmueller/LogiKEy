3d8c47eeac80776cab2d2b6969dec9f2f9879053
lucapasetto <148212465+lucapasetto@users.noreply.github.com>
2026-07-31 16:24:28 +0200

    Update LRP.thy

--- 2026-AAMAS-Data/LRP.thy
+++ 2026-AAMAS-Data/LRP.thy
@@ -1,3 +1,8 @@
+(* 
+Luca Pasetto, Christoph Benzmüller, and Réka Markovich. 2026.
+Formalizing Mental Privacy in LogiKEy.
+*)
+
 section \<open>Right to Privacy in HOL\<close>
 
 text \<open>\<close>
